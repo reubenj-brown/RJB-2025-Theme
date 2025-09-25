14c271b8a08296e47b3c57b1dd59412675554d7a
Reuben J. Brown <reubenjub@gmail.com>
2025-09-24 22:21:45 -0700

    Update page-portfolio.php

--- page-portfolio.php
+++ page-portfolio.php
@@ -343,9 +343,6 @@ get_header('branded'); ?>
     </main>
 
     <!-- Contact Section - Outside main wrapper for full width gradient -->
-        <div class="strategy-intro">
-                <h3 class="serif-font-scaled">My first gig – at two months old – was a starring role in the for “You Were Right” by Badly Drawn Boy. Watch that <a href="https://youtu.be/K1BNOzDnOLI?t=202">here</a>, or contact me for actually serious things below:</h3>
-            </div> 
     <section class="content-section contact-section">
         <div class="contact-content">
             <div class="contact-image">
