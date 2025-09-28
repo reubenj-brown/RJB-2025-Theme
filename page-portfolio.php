ec2248e22f93f28b471df4ff26286d35c6547335
Reuben J. Brown <reubenjub@gmail.com>
2025-09-27 22:24:28 -0700

    Update page-portfolio.php

--- page-portfolio.php
+++ page-portfolio.php
@@ -408,7 +408,7 @@ get_header('branded'); ?>
         </section>
 
             <div class="strategy-intro">
-                <h3 class="serif-font-scaled" id="cv">Here are some resume highlights. Or download a full <a href="https://reubenjbrown.com/wp-content/uploads/2025/08/Reuben-J-Brown_Investigative-Journalist_CV_September-2025.pdf">PDF version</a>.</h3>
+                <h3 class="serif-font-scaled" id="cv">Here are some resume highlights. Or download a full <a href="https://reubenjbrown.com/wp-content/uploads/2025/08/Reuben-J-Brown_Investigative-Journalist_CV_September-2025.pdf">PDF version</a></h3>
             </div> 
         <section class="content-section cv-section">
             <?php echo do_shortcode('[reuben_cv]'); ?>
